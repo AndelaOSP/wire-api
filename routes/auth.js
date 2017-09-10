17a9a5518f2c6354b20cc8ac43a451b5a584676f
patlub <patricklu2010@gmail.com>
2017-09-10 09:06:36 +0300

    Add google auth for validation
    closed #1

--- routes/auth.js
+++ routes/auth.js
@@ -2,51 +2,32 @@ const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const pick = require('lodash/pick');
 
-function auth(req, reply) {
-  // to be updated to use Google Auth
-  const { email, password } = req.payload;
-  // replace Knex with Sequelize
-  const getOperation = Knex('user').where({
-    email
-  }).select('*').then(([user]) => {
-    if (!user) {
-      return reply({
-        error: true,
-        message: 'user not found',
-      }).code(404);
+function auth(req, reply, profile) {
+    if(profile) {
+        if (profile.email.split('@')[1] !== 'andela.com')
+          return reply({'token': 0})
+
+      // extract the relevant data from Profile to store in JWT object
+      var session = {
+        firstname : profile.name.givenName, // the person's first name e.g: Anita
+        image    : profile.image.url,      // profile image url
+        id       : profile.id,             // google+ id
+        email    : profile.email,          // email address
+        exp      : Math.floor(new Date().getTime()/1000) + 7*24*60*60, // Expiry in seconds!
+      }
+      // create a JWT to set as the cookie:
+      var token = JWT.sign(session, process.env.JWT_SECRET);
+
+      // reply to client with a view
+      return reply.redirect('/incidents')
+      .state('token', token);
     }
-
-    // authenticate user user.password = ???
-    if (user.password) {
-      const token = jwt.sign(
-        pick(user, 'name', 'email', 'id'),
-        process.env.JWT_KEY,
-        {
-          algorithm: 'HS256',
-          expiresIn: '150d',
-        }
-      );
-
-      reply({ token, uid: user.uid });
-    } else {
-      reply({ error: true, message: 'incorrect password' }).code(400);
+    else {
+      return reply({'token': 0})
     }
-  }).catch(err => reply({
-    error: true,
-    message: 'server error',
-  }));
-};
-
+  }
 module.exports = {
   method: 'POST',
   path: '/auth',
   handler: auth,
-  config: {
-    validate: {
-      payload: {
-        email: Joi.string().email().required(),
-        password: Joi.string().required(),
-      },
-    },
-  },
 };
