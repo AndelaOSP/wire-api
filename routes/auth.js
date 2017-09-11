1741a8707e0725437b7b87bed493ca4f3ae7d6be
patlub <patricklu2010@gmail.com>
2017-09-11 18:00:39 +0300

    wip

--- routes/auth.js
+++ routes/auth.js
@@ -10,52 +10,24 @@ var passport = require('passport')
 
 function auth(req, reply, profile) {
   // To be put to uri builder
-    googleAuthUri = "https://accounts.google.com/o/oauth2/auth?redirect_uri="
-    + process.env.AUTH_REDIRECT_URL + "&response_type=code&client_id="
-    + process.env.CLIENT_ID +
-    "&scope=https://www.googleapis.com/auth/userinfo.profile+" +
-    "https://www.googleapis.com/auth/userinfo.email&approval_prompt=force&access_type=offline"
+    googleAuthUri = "https://api.andela.com/login?redirect_url=" + process.env.AUTH_REDIRECT_URL
 
-    if(req.query.code == undefined){
+    if(req.query.token == undefined){
         return reply.redirect(googleAuthUri)
     }
 
-    var code = req.query.code;
-    postCode(code);
-  }
+    const token = req.query.token;
+    const payload = jwt.decode(token)
+    console.log(payload);
+    const email = payload.UserInfo.email
 
-  function postCode(code) {
-    // Build the post string from an object
-    var post_data = querystring.stringify({
-        'code' : code,
-        'client_id': process.env.CLIENT_ID,
-        'client_secret': process.env.CLIENT_SECRET,
-        'redirect_uri': process.env.AUTH_REDIRECT_URL,
-        'grant_type': 'authorization_code'
-    });
-
-    // An object of options to indicate where to post to
-    var post_options = {
-        host: 'accounts.google.com',
-        path: '/o/oauth2/token/',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
-    };
-
-    // Set up the request
-    var post_req = http.request(post_options, function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('Response: ' + chunk);
-        });
-    });
-
-    // post the data
-    post_req.write(post_data);
-    post_req.end();
-
+    if(email.split('@')[1] !== 'andela.com'){
+      return reply({auth: 0})
+    }
+    const id = payload.UserInfo.id
+    const firstName = payload.UserInfo.first_name
+    const lastName = payload.UserInfo.last_name
+    const picture = payload.UserInfo.picture
   }
 
 module.exports = {
