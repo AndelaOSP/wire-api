f3624c6c6a59db9100f34e627c3d4d4922ffc19c
Mercy-Muchai <mercy.muchai@andela.com>
2017-11-20 18:12:33 +0300

    clean up mysql db folder

--- db/dumps/wire-dev-20170910.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `incident` 
-DROP FOREIGN KEY `incident_ibfk_4`;
-ALTER TABLE `incident` 
-CHANGE COLUMN `status_id` `status_id` INT(11) NULL DEFAULT 1 COMMENT '' ;
-ALTER TABLE `incident` 
-ADD CONSTRAINT `incident_ibfk_4`
-  FOREIGN KEY (`status_id`)
-  REFERENCES `incident_status` (`id`);
