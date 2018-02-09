eb97077539ae9979f8c9004ac76eba28e617f4ec
Mnickii <caroline.nkirote@andela.com>
2018-02-09 10:05:14 +0300

    feat(assignee-model): Create an assignee association for Incidents 	-Add Assignee model 	-Add assignee relations in User and Incident model 	-Update status seeders to match mock-up
    [Delivers #154922122](https://www.pivotaltracker.com/story/show/154922122)
    
    Signed-off-by: Mnickii <caroline.nkirote@andela.com>

--- server/seeders/status.js
+++ server/seeders/status.js
@@ -3,7 +3,7 @@
 module.exports = {
   up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('Statuses', [{
-      status: 'Open',
+      status: 'Pending',
       createdAt: new Date().toISOString(),
       updatedAt: new Date().toISOString() 
     },
