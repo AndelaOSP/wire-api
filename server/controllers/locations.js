7b85a373d1aeaf09da62b1d31ca0d81050aee9d3
Mercy-Muchai <mercy.muchai@andela.com>
2018-03-22 12:51:35 +0300

    chore(refactor controllers): - rm services folder - update locations & users to user findOrCreate - update incidents create method - remove POST locations endpoint - format code
    Signed-off-by: Mercy-Muchai <mercy.muchai@andela.com>

--- server/controllers/locations.js
+++ server/controllers/locations.js
@@ -1,88 +1,90 @@
-const locationService = require("../services/locations");
-const Location = require("../models").Locations;
-const Incident = require("../models").Incidents;
+const Location = require('../models').Locations;
+const Incident = require('../models').Incidents;
 
 module.exports = {
-  // add a location
-  create(req, res) {
-    name = req.body.name,
-      centre = req.body.centre,
-      country = req.body.country
-    return locationService.create(name, centre, country).then(location => {
-      if (location === "Resolved") {
-        return res.status(200).send({ message: "This location already exists" })
-      }
-      if (location === "Reject") {
-        return res.status(400).send({ message: "Please enter (name, centre, country)" })
-      }
-      res.status(201).send({ data: location, status: "success" });
-    }).catch(error => {
-      res.status(400).send(error);
-    });
-  },
+	// add a location
+	create(location, res) {
+		let { name, centre, country } = location;
+		if (!name || !centre || !country) {
+			return res.status(400).send({
+				status: 'fail',
+				message: 'Provide the location name, centre and country',
+			});
+		}
+		return Location.findOrCreate({
+			where: {
+				name,
+				centre,
+				country,
+			},
+		})
+			.spread((location, created) => {
+				return location;
+			})
+			.catch(error => {
+				res.status(400).send(error);
+			});
+	},
 
-  // view all locations
-  list(req, res) {
-    return Location
-    .findAll()
-    .then(location => {
-      res.status(200).send({ data: { locations: location }, status: "success" });
-    })
-    .catch(error => {
-      res.status(400).send(error)
-    });
-  },
+	// view all locations
+	list(req, res) {
+		return Location.findAll()
+			.then(location => {
+				res.status(200).send({ data: { locations: location }, status: 'success' });
+			})
+			.catch(error => {
+				res.status(400).send(error);
+			});
+	},
 
-  // retrieve a location by ID
-  findById(req, res) {
-    return Location
-      .findById(req.params.id)
-      .then(location => {
-        if (!location) {
-          return res.status(404).send({
-            message: 'Location not found', status: "fail"
-          });
-        }
-        res.status(200).send({ data: location, status: "success" });
-      })
-      .catch(error => {
-        res.status(400).send(error);
-      });
-  },
+	// retrieve a location by ID
+	findById(req, res) {
+		return Location.findById(req.params.id)
+			.then(location => {
+				if (!location) {
+					return res.status(404).send({
+						message: 'Location not found',
+						status: 'fail',
+					});
+				}
+				res.status(200).send({ data: location, status: 'success' });
+			})
+			.catch(error => {
+				res.status(400).send(error);
+			});
+	},
 
-  // update a location
-  update(req, res) {
-    return Location
-      .findById(req.params.id)
-      .then(location => {
-        if (!location) {
-          return res.status(404).send({
-            message: 'Location not found', status: "fail"
-          });
-        }
-        return location
-          .update({
-            name: req.body.name || location.name,
-            centre: req.body.centre || location.centre,
-            country: req.body.country || location.country
-          })
-          .then(() => res.status(200).send({ data: location, status: "success" }))
-          .catch(error => res.status(400).send(error));
-      });
-  },
-  // filter incidents by location
-  listIncidents(req, res) {
-    return Incident
-      .findAll({
-        where: {
-          locationId: req.params.id
-        },
-      })
-      .then(incident => {
-        res.status(200).send({ data: { incidents: incident }, status: "success" });
-      })
-      .catch(error => {
-        res.status(400).send(error)
-      });
-  },
-}
+	// update a location
+	update(req, res) {
+		return Location.findById(req.params.id).then(location => {
+			if (!location) {
+				return res.status(404).send({
+					message: 'Location not found',
+					status: 'fail',
+				});
+			}
+			return location
+				.update({
+					name: req.body.name || location.name,
+					centre: req.body.centre || location.centre,
+					country: req.body.country || location.country,
+				})
+				.then(() => res.status(200).send({ data: location, status: 'success' }))
+				.catch(error => res.status(400).send(error));
+		});
+	},
+	// filter incidents by location
+	listIncidents(req, res) {
+		return Incident.findAll({
+			where: {
+				locationId: req.params.id,
+			},
+		})
+			.then(incident => {
+				res.status(200).send({ data: { incidents: incident }, status: 'success' });
+			})
+			.catch(error => {
+				res.status(400).send(error);
+			});
+	},
+};
