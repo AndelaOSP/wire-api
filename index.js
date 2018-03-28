18eb1329921c799e9571248704acfe42e2c8048d
Mercy-Muchai <mercy.muchai@andela.com>
2018-03-28 12:54:08 +0300

    configure cors
    Signed-off-by: Mercy-Muchai <mercy.muchai@andela.com>

--- index.js
+++ index.js
@@ -11,7 +11,28 @@ if (NODE_ENV !== 'development') {
 
 // set up the express app
 const app = express();
+
 app.use(cors());
+
+let allowedOrigins = [
+  'https://wire-front-staging.herokuapp.com',
+  'https://wireapi-develop.herokuapp.com',
+  'wire.andela.com:8080',
+  'https://wirebot-testing.herokuapp.com',
+  'https://wire-bot.herokuapp.com/'
+];
+
+app.use((req, res, next) => {
+  let origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) >= 0){
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type'
+  );
+  next();
+});
 
 // Log requests to the console.
 app.use(logger('dev'));
@@ -23,8 +44,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Require routes
 require('./server/routes/index')(app);
 // Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to WIRE.',
-}));
+app.get('*', (req, res) =>
+  res.status(200).send({
+    message: 'Welcome to WIRE.'
+  })
+);
 
 module.exports = app;
