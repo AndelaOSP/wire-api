c9ed63dbe36e84251f8f96a7cf345a16c5be3134
Rachael Njeri <rachael.njeri@andela.com>
2018-04-05 18:08:45 +0300

    Add users controllers tests
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- server/controllers/chats.js
+++ server/controllers/chats.js
@@ -1,75 +1,97 @@
 const Chat = require('../models').Chats;
 const User = require('../models').Users;
 
+let userAttributes = {
+  model: User,
+  attributes: ['id', 'imageUrl', 'username']
+};
+
+const findChatById = (id, res) => {
+  return Chat.findById(id, { include: userAttributes })
+    .then(chat => {
+      if (!chat) {
+        return res.status(404)
+          .send({ message: 'Chat not found', status: 'fail' });
+      }
+      return chat;
+    })
+    .catch(error => {
+      res.status(400).send(error);
+    });
+};
+
 module.exports = {
   // add a Chat
   create(req, res) {
-    const { chat } = req.body;
-    return Chat.findOne({ where: { chat } })
+    return Chat.create({
+      chat: req.body.chat,
+      userEmail: req.body.userEmail,
+      incidentId: req.params.id
+    })
       .then(chat => {
-        return Chat.create({
-          chat: req.body.chat,
-          userEmail: req.body.userEmail,
-          incidentId: req.params.id
-        }).then(chat => {
-          return res.status(201).send({ data: chat, status: 'success' });
-        });
+        return findChatById(chat.id, res);
+      })
+      .then(data => {
+        return res.status(201).send({ data, status: 'success' });
       })
       .catch(error => {
         res.status(400).send(error);
       });
   },
+
   // view all Chats of a Note
   list(req, res) {
     return Chat.findAll({
       where: {
         incidentId: req.params.id
-      }
+      },
+      include: userAttributes
     })
       .then(chat => {
-        return res.status(200).send({ data: { chats: chat }, status: 'success' });
+        return res
+          .status(200)
+          .send({ data: { chats: chat }, status: 'success' });
       })
       .catch(error => {
         res.status(400).send(error);
       });
   },
+
   // retrieve a Chat by ID
   findById(req, res) {
-    return Chat.findById(req.params.id)
-      .then(Chat => {
-        if (!Chat) {
-          return res.status(404).send({
-            message: 'Chat not found',
-            status: 'fail'
-          });
-        }
-        return res.status(200).send({ data: Chat, status: 'success' });
+    return findChatById(req.params.id, res)
+      .then(chat => {
+        return res.status(200).send({ data: chat, status: 'success' });
       })
       .catch(error => {
         res.status(400).send(error);
       });
   },
+
   // update a Chat
   update(req, res) {
-    return Chat.findById(req.params.id)
-      .then(Chat => {
-        if (!Chat) {
-          return res.status(404).send({
-            message: 'Chat not found',
-            status: 'fail'
-          });
-        }
-        return Chat.update({
-          chat: req.body.chat || Chat.chat,
-          incidentId: req.body.incidentId || Chat.incidentId,
-          userEmail: req.body.userEmail || Chat.userEmail
+    return Chat.findById(req.params.id).then(Chat => {
+      if (!Chat) {
+        return res.status(404).send({
+          message: 'Chat not found',
+          status: 'fail'
+        });
+      }
+      return Chat.update({
+        chat: req.body.chat || Chat.chat,
+        incidentId: req.body.incidentId || Chat.incidentId,
+        userEmail: req.body.userEmail || Chat.userEmail
+      })
+        .then(Chat => {
+          return findChatById(Chat.id, res);
         })
-          .then(Chat => {
-            return res.status(200).send({ data: Chat, status: 'success' });
-          })
-          .catch(error => res.status(400).send(error));
-      });
+        .then(data => {
+          return res.status(200).send({ data, status: 'success' });
+        })
+        .catch(error => res.status(400).send(error));
+    });
   },
+
   // delete a Chat
   delete(req, res) {
     return Chat.findById(req.params.id).then(Chat => {
