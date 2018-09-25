f734315977239fd7e9a03e2e30ce274f6c83f6ba
kimobrian <brian.kimokoti@andela.com>
2018-09-25 16:35:33 +0300

    chore: Fix invite issue

--- src/server/helpers/getUsernameFromEmail.js
+++ src/server/helpers/getUsernameFromEmail.js
@@ -8,6 +8,6 @@ module.exports = email => {
   let name = {};
   const [firstName, lastName] = email.split('@')[0].split('.');
   const first = firstName[0].toUpperCase() + firstName.substr(1, firstName.length);
-  const last = lastName[0].toUpperCase() + lastName.substr(1, lastName.length);
+  const last = lastName ? lastName[0].toUpperCase() + lastName.substr(1, lastName.length): '';
   return name = {first, last};
 };
