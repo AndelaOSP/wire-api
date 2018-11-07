11a44672ed09060a826bae13f7bb24d6302a9ea3
basedHokage <eugene.omar@andela.com>
2018-11-07 20:14:50 +0300

    [ft-update-ccd-user-message-160482344] add the current logged in user as a param for when adding a ccd user

--- src/server/helpers/incidentHelper.js
+++ src/server/helpers/incidentHelper.js
@@ -59,6 +59,7 @@ const sendAssigneeOrCcdEmail = async payload => {
   const userDetails = await getUserDetails(payload);
   const emailBody = await generateAssigneeOrCcdEmailBody({
     ...userDetails.dataValues,
+    tagger: payload.tagger,
     assignedRole: payload.assignedRole // add assigned role
   });
   emailHelper.sendMail(emailBody, error => {
@@ -101,10 +102,10 @@ const addAssignee = ({ assignedUser, incident, res }) => {
  * @param incident object
  * @return data and success message
  */
-const addCcdUser = ({ ccdUser, res, incident}) => {
+const addCcdUser = ({ ccdUser, res, incident, tagger}) => {
   const ccdPromises = ccdUser.map(async user => {
-    const ccd = User.findById(user.userId);
-    let currentCcd = { ...user, assignedRole: 'ccd' };
+    const ccd = await User.findById(user.userId);
+    let currentCcd = { ...user, assignedRole: 'ccd', tagger};
     await sendAssigneeOrCcdEmail(currentCcd);
     ccd.assigneeIncidents = {
       assignedRole: 'ccd'
