6d407042f779f901ba2e64784868c0b2bd6abe9d
basedHokage <eugene.omar@andela.com>
2018-12-04 18:44:02 +0300

    [add-request-validation-162382625] add the various middlewares to a single file to make importing the easier

---
+++ src/server/middlewares/index.js
@@ -0,0 +1,15 @@
+const validateIncidentPayload = require('./incidents/validateIncidentPayload');
+const validateIncidentId = require('./incidents/validateIncidentId');
+const validateUserPayload = require('./users/validateUserPayload');
+const validateUserId = require('./users/validateUserId');
+const validateNotePayload = require('./notes/validateNotePayload');
+const validateNoteId = require('./notes/validateNoteId');
+
+module.exports = {
+  validateIncidentPayload,
+  validateIncidentId,
+  validateUserPayload,
+  validateUserId,
+  validateNotePayload,
+  validateNoteId
+};
