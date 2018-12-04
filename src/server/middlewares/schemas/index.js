2161a7af9ae72116cab5f0b5bbbe008f5866c630
basedHokage <eugene.omar@andela.com>
2018-12-04 18:43:14 +0300

    [add-request-validation-162382625] add the various schemas to a single file to avaoid duplication

---
+++ src/server/middlewares/schemas/index.js
@@ -0,0 +1,65 @@
+const Joi = require('joi');
+
+const locationSchema = {
+  name: Joi.string().required(),
+  centre: Joi.string().required(),
+  country: Joi.string().required()
+};
+
+const userSchema = {
+  userId: Joi.string().required(),
+  email: Joi.string()
+    .email()
+    .required(),
+  username: Joi.string().required(),
+  imageUrl: Joi.string()
+};
+
+const incidentReporterSchema = {
+  ...userSchema,
+  reporterLocation: locationSchema
+};
+
+const witnessSchema = {
+  ...userSchema,
+  witnessLocation: locationSchema
+};
+
+const noteSchema = {
+  note: Joi.string().required(),
+  userEmail: Joi.string()
+    .email()
+    .required()
+};
+
+const incidentSchema = {
+  id: Joi.string(),
+  statusId: Joi.number(),
+  subject: Joi.string().required(),
+  description: Joi.string().required(),
+  dateOccurred: Joi.date().required(),
+  levelId: Joi.string().required(),
+  location: locationSchema,
+  incidentReporter: incidentReporterSchema,
+  witnesses: Joi.array()
+    .items(witnessSchema)
+    .required()
+};
+
+const { userId, ...rest } = userSchema;
+
+const newUserSchema = {
+  ...rest,
+  roleId: Joi.number().required(),
+  location: locationSchema
+};
+
+module.exports = {
+  locationSchema,
+  userSchema,
+  incidentReporterSchema,
+  witnessSchema,
+  incidentSchema,
+  newUserSchema,
+  noteSchema
+};
