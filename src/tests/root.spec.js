87b98877c407217c0c8a7dac99efb789fb06d637
Oliver Munala <munalaoliverngoitsi@gmail.com>
2018-12-17 20:36:29 +0300

    fix(linting): fix codeclimate issues

--- src/tests/root.spec.js
+++ src/tests/root.spec.js
@@ -18,6 +18,15 @@ const cannotViewIncidentsToken = token({
   username: 'Batian Muthoga',
 });
 
+const makeRequest = (done, message, token) => {
+  let getRequest = request(app).get('/');
+  if (token) getRequest = getRequest.set('Authorization', token);
+  getRequest.expect(401).end((err, res) => {
+    expect(res.body.message).toEqual(message);
+    done();
+  });
+};
+
 describe('Root tests', () => {
   it('should return welcome message', done => {
     sendRequest('get', '', null, (err, res) => {
@@ -28,51 +37,28 @@ describe('Root tests', () => {
 
   it('should return login message', done => {
     sendRequest('get', '/api', null, (err, res) => {
-      expect(res.body.message).toEqual('Log an incident on WIRE');
+      expect(res.body.message).toBe('Log an incident on WIRE');
       done();
     });
   });
 
   it('should return error if token is not provided', done => {
-    request(app)
-      .get('/')
-      .expect(401)
-      .end((err, res) => {
-        expect(res.body.message).toEqual('No token provided');
-        done();
-      });
+    makeRequest(done, 'No token provided');
   });
 
   it('should return error if token is invalid', done => {
-    request(app)
-      .get('/')
-      .set('Authorization', 'invalid token')
-      .expect(401)
-      .end((err, res) => {
-        expect(res.body.message).toEqual('Invalid token provided');
-        done();
-      });
+    makeRequest(done, 'Invalid token provided', 'invalid token');
   });
 
   it('should return error if user is not authorized on admin endpoints', done => {
-    request(app)
-      .get('/api/users')
-      .set('Authorization', notAdminToken)
-      .expect(401)
-      .end((err, res) => {
-        expect(res.body.message).toEqual('You are not an Authorised user');
-        done();
-      });
+    makeRequest(done, 'You are not an Authorised user', notAdminToken);
   });
 
   it('should return error if user is not authorized to view incidents', done => {
-    request(app)
-      .get('/api/incidents')
-      .set('Authorization', cannotViewIncidentsToken)
-      .expect(401)
-      .end((err, res) => {
-        expect(res.body.message).toEqual('You are not an Authorised user');
-        done();
-      });
+    makeRequest(
+      done,
+      'You are not an Authorised user',
+      cannotViewIncidentsToken
+    );
   });
 });
