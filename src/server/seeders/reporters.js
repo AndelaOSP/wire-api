e92ee9fe4454356576d2a1c4da41b3387024a76f
Derrick Kirwa <dkip64@gmail.com>
2019-03-21 13:05:59 +0300

    bug(seeders): add reporters to incidents
    - add reporters to incidents
    
    [Fixes #164795511]

--- src/server/seeders/reporters.js
+++ src/server/seeders/reporters.js
@@ -1,25 +1,8 @@
+const reporters = require('../utils/data/reporters.json');
+
 module.exports = {
   up(queryInterface) {
-    return queryInterface.bulkInsert('userIncidents', [
-      {
-        incidentId: 'cjfkubrlv0001tgxs3mre',
-        userId: 'cjl6ege6e000053nyv67otq7a',
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString(),
-      },
-      {
-        incidentId: 'cjfkubrlv0001tgxs3',
-        userId: 'cjl6efcka00004tny9ilz7b61',
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString(),
-      },
-      {
-        incidentId: 'cjfkubrlv0001tsjksuis3',
-        userId: 'cjl6efcka00004tny9ilz7b61',
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString(),
-      },
-    ]);
+    return queryInterface.bulkInsert('userIncidents', reporters.reporters);
   },
 
   down(queryInterface) {
