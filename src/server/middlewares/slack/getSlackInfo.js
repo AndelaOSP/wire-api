2f5e34d9a797265520df04b7415a1f832a529b27
rethes <rethesmutua@gmail.com>
2019-03-28 17:24:44 +0300

    feat(chats): Store chats from slack
    -Add a create chat endpoints
    -Add models and migrations
    -Add tests for the implementation
    
    [Finishes #164958037]

---
+++ src/server/middlewares/slack/getSlackInfo.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const models = require('../../models/index');
+
+async function createSlackUser({user: {id, profile}}) {
+  const slackUser = await models.slackUsers.findById(id);
+
+  if (!slackUser) {
+    models.slackUsers.create({
+      id,
+      username: profile.real_name,
+    });
+  }
+
+}
+
+async function getSlackUsers(userId) {
+  const res = await axios.get('https://slack.com/api/users.info?token=' + process.env.SLACK_TOKEN + '&user=' + userId + '&pretty=1');
+  let payload = res.data;
+
+  if (!payload.error) {
+    await createSlackUser(payload);
+  }
+
+  return payload.error;
+}
+
+const getSlackChannel = (channelId, userId) => {
+  axios.get('https://slack.com/api/channels.info?token=' + process.env.SLACK_TOKEN + '&channel=' + channelId + '&pretty=1')
+    .then(res => {
+      let payload = res.data;
+
+      if (!payload.error) {
+        models.slackUsers.findById(userId).then(slackUser2 => {
+          if (slackUser2) {
+            slackUser2.update({
+              channelName: payload.channel.name,
+            });
+          }
+        })
+      }
+
+      return payload.error;
+    })
+};
+
+module.exports = {getSlackUsers, getSlackChannel};
---
+++ src/tests/slackEvents.spec.js
@@ -0,0 +1,103 @@
+const {sendRequest} = require('./helpers/request');
+
+const testSlackEvent = {
+  "event": {
+    "type": "message",
+    "text": "For inline help type wire chat help at any time any place",
+    "user": "UHC32KE69",
+    "ts": "1554121824.018400",
+    "channel": "CGXDP5NQM",
+    "event_ts": "1554121824.018400",
+    "channel_type": "channel"
+  },
+  "event_id": "EvHJD54099",
+  "event_time": "1554121824",
+};
+
+const testSlackEvent2 = {
+  "event": {
+    "type": "message",
+    "text": "Tokens for Your Workspace",
+    "user": "UGYGTEMTM",
+    "ts": "1554121824.018400",
+    "channel": "1234567",
+    "event_ts": "1554121824.018400",
+    "channel_type": "channel"
+  },
+  "event_id": "EvHJD54097",
+  "event_time": "1554121824",
+};
+
+const testSlackEvent3 =
+  {
+    token: 'gUevdAHWJG6rKHWyjo3aaT7T',
+    team_id: 'TGX3UHAM8',
+    api_app_id: 'AHC1KDL5B',
+    event:
+      {
+        client_msg_id: 'b35c818b-3434-4965-b033-b95007f96da3',
+        type: 'message',
+        text: 'Twitter/Facebook style',
+        user: 'UGYGTEMTQ',
+        ts: '1554828958.031500',
+        channel: 'CGXDP5NQM',
+        event_ts: '1554828958.031500',
+        channel_type: 'channel'
+      },
+    type: 'event_callback',
+    event_id: 'EvHQFNGJE5',
+    event_time: 1554828958,
+    authed_users: ['UHC32KE69']
+  };
+
+
+const testUrlVerification = {
+  "token": "Jhj5dZrVaK7ZwHHjRyZWjbDl",
+  "challenge": "3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P",
+  "type": "url_verification"
+};
+
+const slackEventEndpoint = '/api/slack/chats';
+
+describe('SlackEvent Tests', () => {
+
+  it('should return a response for urlVerification', done => {
+    sendRequest('post', slackEventEndpoint, testUrlVerification, (err, res) => {
+      expect(res.body.data).toHaveProperty('type');
+      expect(res.body.status).toMatch('success');
+      done();
+    });
+  });
+
+  it('should create a SlackEvent given the correct payload', done => {
+    sendRequest('post', slackEventEndpoint, testSlackEvent, (err, req) => {
+      setTimeout(() => {
+        expect(req.body.status).toEqual('success');
+        done();
+      }, 4000);
+    });
+  });
+
+  it('should not create a SlackEvent given the wrong payload', done => {
+    sendRequest('post', slackEventEndpoint, testSlackEvent2, (err, req) => {
+      setTimeout(() => {
+        expect(req.body.status).toEqual('failure');
+        done();
+      }, 4000);
+    });
+  });
+
+  it('should not create a SlackEvent twice', done => {
+    sendRequest('post', slackEventEndpoint, testSlackEvent, (err, req) => {
+      expect(req.body.message).toEqual('id must be unique');
+      done();
+    });
+  });
+
+  it('should create a SlackEvent given a new user', done => {
+    sendRequest('post', slackEventEndpoint, testSlackEvent3, (err, req) => {
+      expect(req.body.status).toEqual('success');
+      done();
+    });
+  });
+});
